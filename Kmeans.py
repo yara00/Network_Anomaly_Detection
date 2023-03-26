307ccce8c12f18a5a856504717a7c7b028a8d5fc
yarahossamboo <yarahossam222@gmail.com>
2023-03-26 23:51:34 +0200

    Update Kmeans.py

--- Kmeans.py
+++ Kmeans.py
@@ -22,7 +22,6 @@ class Kmeans:
         for i in range(self.k):
             dist = [(a - b) ** 2 for a, b in zip(current[i], prev[i])]
             dist_sum += np.asarray(dist).sum()
-        print(dist_sum)
         return dist_sum
 
     def __randomize_centroids(self, k):
@@ -33,7 +32,6 @@ class Kmeans:
         return centroids
 
     def __assign_clusters(self, centroids, k):
-        print("Assigning clusters..")
         self.clusters = [[] for j in range(k)]
         similarities = cdist(self.data_matrix, np.asarray(centroids).reshape(k, -1), 'euclidean')
         idx = np.argmin(similarities, axis=1)
@@ -41,10 +39,8 @@ class Kmeans:
             self.clusters[idx[i]].append(i)
 
     def __update_centroids(self, k):
-        print("Updating centroids..")
         centroids = [[] for i in range(k)]
         for i in range(k):
-            print("LEN ", len(self.clusters[i]))
             cluster_sum = np.zeros((1, 41))
             if len(self.clusters[i]) == 0:
                 rnd = randrange(0, self.data_matrix.shape[0])
@@ -57,32 +53,29 @@ class Kmeans:
 
     def algorithm(self):
         k = self.k
-        print("K", k)
         iterations = 0
         centroids = self.__randomize_centroids(k)
         prev_centroids = np.zeros(np.array(centroids).shape)
         while (self.__distance(centroids, prev_centroids) != 0) and (iterations < 1000):
             iterations += 1
-            print("Iteration no: ", iterations)
             self.__assign_clusters(centroids, k)
             prev_centroids = centroids
             centroids = self.__update_centroids(k)
-
+        print("Iterations no: ", iterations)
         return self.clusters, centroids
 
 
 if __name__ == '__main__':
-    # 7, 15, 23, 31, 45
-    k = 45
-    start = time.time()
-    filename = 'kmeans_' + str(k) + '.csv'
-    cluster, centroid = Kmeans(Dataset().generate_data_matrix(), k).algorithm()
-    end = time.time()
-    print("time: ")
-    print((end - start) / 60)
-    file = open(filename, 'w', newline='')
-
-    with file:
-        write = csv.writer(file)
-        write.writerow([k])
-        write.writerows(centroid)
+    k_mat = [7, 15, 23, 31, 45]
+    for k in k_mat:
+        print("K = ", k)
+        start = time.time()
+        filename = 'kmeans_' + str(k) + '.csv'
+        cluster, centroid = Kmeans(Dataset().generate_data_matrix(), k).algorithm()
+        end = time.time()
+        print("Elapsed Time: " + str((end - start) / 60) + " mins")
+        file = open(filename, 'w', newline='')
+        with file:
+            write = csv.writer(file)
+            write.writerow([k])
+            write.writerows(centroid)
